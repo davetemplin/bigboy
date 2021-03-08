6a6fcfafaf3d672aa758c9d31ff6906a321af25d
Igor Starostenko <igor.star@aol.com>
2021-03-08 00:15:41 -0800

    refactored parseArgs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	_ "net/http/pprof"
@@ -9,7 +10,8 @@ import (
 	"time"
 )
 
-var args struct {
+// Args - Command Line Arguments
+type Args struct {
 	errors  uint64
 	nulls   bool
 	out     string
@@ -22,11 +24,22 @@ var args struct {
 	version bool
 	workers int
 }
+
+var args Args
 var errors uint64
 
 func main() {
 	loadConfig("config.json")
-	parseArgs()
+	args, _ := parseArgs(os.Args[0], os.Args[1:])
+
+	if args.version {
+		stop(version, 0)
+	}
+
+	if args.target == "" {
+		usage()
+		os.Exit(0)
+	}
 
 	t := time.Now()
 	target := loadTarget()
@@ -48,29 +61,36 @@ func main() {
 	}
 }
 
-func parseArgs() {
-	flag.Uint64Var(&args.errors, "e", config.Errors, "max errors allowed")
-	flag.BoolVar(&args.nulls, "n", config.Nulls, "Include nulls in output")
-	flag.StringVar(&args.out, "o", "", "Output file or directory")
-	flag.IntVar(&args.page, "p", config.Page, "Rows extracted per query")
-	flag.BoolVar(&args.quiet, "q", config.Quiet, "Supress informational output")
-	flag.Uint64Var(&args.retries, "r", config.Retries, "max consecutive errors")
-	flag.BoolVar(&args.version, "v", false, "Print version info about bigboy and exit")
-	flag.IntVar(&args.workers, "w", config.Workers, "# of workers")
-	flag.Usage = usage
-	flag.Parse()
+func parseArgs(progname string, input []string) (args *Args, output string) {
+	flags := flag.NewFlagSet(progname, flag.ContinueOnError)
+	var buf bytes.Buffer
+	flags.SetOutput(&buf)
 
-	if args.version {
-		stop(version, 0)
-	}
+	var a Args
+	flags.Uint64Var(&a.errors, "e", config.Errors, "max errors allowed")
+	flags.BoolVar(&a.nulls, "n", config.Nulls, "Include nulls in output")
+	flags.StringVar(&a.out, "o", "", "Output file or directory")
+	flags.IntVar(&a.page, "p", config.Page, "Rows extracted per query")
+	flags.BoolVar(&a.quiet, "q", config.Quiet, "Supress informational output")
+	flags.Uint64Var(&a.retries, "r", config.Retries, "max consecutive errors")
+	flags.BoolVar(&a.version, "v", false, "Print version info about bigboy and exit")
+	flags.IntVar(&a.workers, "w", config.Workers, "# of workers")
+	flags.Usage = usage
 
-	if len(flag.Args()) == 0 {
-		usage()
+	err := flags.Parse(input)
+	if err != nil {
+		fmt.Println("Error reading arguments", err)
 		os.Exit(0)
 	}
 
-	args.target = flag.Args()[0]
-	args.params = flag.Args()[1:len(flag.Args())]
+	allArgs := flags.Args()
+	if len(allArgs) == 0 {
+		return &a, buf.String()
+	}
+
+	a.target = allArgs[0]
+	a.params = allArgs[1:]
+	return &a, buf.String()
 }
 
 func usage() {
