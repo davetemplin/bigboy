b9ae729c82e1f1d1d3e2383823e6c97b98b7e851
Igor Starostenko <igor.star@aol.com>
2021-03-11 22:42:58 -0800

    go install in actions

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 trap "exit" ERR
 
+# prod
 go get github.com/denisenkom/go-mssqldb
 go get github.com/lib/pq
 go get github.com/go-sql-driver/mysql
 go get golang.org/x/crypto/md4 # required if cross building from windows
+
+# dev
+go get github.com/stretchr/testify/assert
