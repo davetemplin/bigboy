4de33953b9bd3486c7e548fde07583f91c4fa8d8
Igor Starostenko <igor.star@aol.com>
2021-03-20 00:49:27 -0700

    Test resolveTarget() with go-sqlmock

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bigboy
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/denisenkom/go-mssqldb v0.9.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/lib/pq v1.10.0
--- target_test.go
+++ target_test.go
@@ -6,10 +6,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 )
 
-func CreateTarget(path string, json string) {
+func CreateFile(path string, json string) {
 	f, err := os.Create(path)
 	if err != nil {
 		panic(err)
@@ -24,7 +25,7 @@ func CreateTarget(path string, json string) {
 	}
 }
 
-func DeleteTarget(path string) {
+func DeleteFile(path string) {
 	err := os.Remove(path)
 	if err != nil {
 		panic(err)
@@ -32,25 +33,45 @@ func DeleteTarget(path string) {
 }
 
 func TestLoadTargetEmpty(t *testing.T) {
-	targetPath := "test"
-	filePath := path.Join(targetPath, defaultTarget)
-	CreateTarget(filePath, "{}")
+	args = Args{target: "test"}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	connections := make(map[string]*Connection)
+	connectionName := "default"
+	connections[connectionName] = &Connection{
+		Driver: "mysql",
+		db:     db,
+	}
+	config = Config{Connections: connections}
+
+	targetPath := path.Join(args.target, defaultTarget)
+	CreateFile(targetPath, "{}")
+	extractPath := path.Join(args.target, defaultExtract)
+	extractConfig := "{}"
+	CreateFile(extractPath, extractConfig)
+
+	mock.ExpectBegin()
 
 	target := loadTarget()
 
-	assert.Equal(t, "", target.Connection, "Target Connection set")
-	assert.Equal(t, "", target.Fetch, "Target Fetch set")
+	assert.Equal(t, connectionName, target.Connection, "Target Connection set")
+	assert.Equal(t, defaultExtract, target.Fetch, "Target Fetch set")
 	assert.Equal(t, []Param(nil), target.Params, "Target Params set")
 	assert.Equal(t, false, target.Prefetch, "Target Prefetch set")
 	assert.Equal(t, []*Nest(nil), target.Nest, "Target Prefetch set")
 	assert.Equal(t, "", target.Script, "Target Script set")
 	assert.Equal(t, (*Split)(nil), target.Split, "Target Split set")
 	assert.Equal(t, "", target.Timezone, "Target Timezone set")
-	assert.Equal(t, (*Connection)(nil), target.connection, "Target connection set")
-	assert.Equal(t, "", target.extract, "Target extract set")
+	assert.Equal(t, connections[connectionName], target.connection, "Target connection set")
+	assert.Equal(t, extractConfig, target.extract, "Target extract set")
 	assert.Equal(t, "", target.prefetch, "Target prefetch set")
 	assert.Equal(t, []interface{}(nil), target.params, "Target params set")
 	assert.Equal(t, (*time.Location)(nil), target.location, "Target location set")
 
-	DeleteTarget(filePath)
+	DeleteFile(targetPath)
 }
